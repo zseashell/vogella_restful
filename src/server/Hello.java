e9e559b25e1da2c09474756333b89b7644e78eda
seashell <seashell@cienet.com.cn>
2014-09-29 19:31:35 +0800

    initial commit

---
+++ src/server/Hello.java
@@ -0,0 +1,30 @@
+package server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey </hello>";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHTMLHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>" + "<body><h1>" + "Hello Jersey" + "</h1></body>"
+            + "</html>";
+    }
+
+}
